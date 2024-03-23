98f55556bf9990ebcb6b03bdb6b02580a7a426fd
Anoop Kuttikattu Sukumaran Nair <anpks@Anoops-Air.box>
2024-03-23 16:48:08 +0100

    Commit after adding react-redux to the app

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,27 @@
 import classes from './Counter.module.css';
+import {useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+  const counter = useSelector((state)=> state.counter);
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
+
+  function handleIncrement(){
+    dispatch({type: "increment"});
+  };
+
+  function handleDecrement(){
+    dispatch({type: "decrement"});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>
+        <button onClick={handleIncrement}>+</button>
+        {counter}
+        <button onClick={handleDecrement}>-</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+
+const createReducer = (state={counter:0}, action)=>{
+
+    if(action.type==="increment"){
+        return {
+            counter: state.counter + 1
+        };
+    }
+
+    if(action.type==="decrement"){
+        return {
+            counter: state.counter - 1
+        };
+    }
+
+    return state;
+}
+
+const store = createStore(createReducer);
+
+export default store;
